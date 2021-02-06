8953a591894494a0394dccefcdca5a58697215ec
Ricardo1335 <ricardo.amaya@mail.escuelaing.edu.co>
2021-02-06 10:28:33 -0500

    fin

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -1,9 +1,29 @@
 package edu.eci.cvds.tdd.registry;
 
-public class Registry {
-    public RegisterResult registerVoter(Person p) {
+import java.util.ArrayList;
 
-        // TODO Validate person and return real result.
-        return RegisterResult.VALID;
-    }
+public class Registry {
+	private ArrayList<Integer> Id = new ArrayList<Integer>();
+	private boolean flag = false;
+    public RegisterResult registerVoter(Person p) {
+	
+		if (Id.contains(p.getId())){
+			return RegisterResult.DUPLICATED;
+		}else {
+			Id.add(p.getId());
+			if (!p.isAlive()) {
+				return RegisterResult.DEAD;
+			}else {
+				if (0>p.getAge() || 130<p.getAge()) {
+					return RegisterResult.INVALID_AGE;
+				}else {
+					if(0<=p.getAge() && p.getAge()<18) {
+						return RegisterResult.UNDERAGE;
+					}else {
+						return RegisterResult.VALID;
+					}
+				}
+			}
+		}    	
+	}
 }
